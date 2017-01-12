6c09de0b91d45cf166332e684e422603a707dbe4
turbodivx <dawid.sielski@outlook.com>
2017-01-12 17:45:26 +0100

    Moving player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    private void Update() //called every frame
+    {
+        
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() //called every phisical operation
+    {
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+
+        rb.AddForce(movement * speed);
+    }
+}
